26b41a6bdb11aff092397b75179ca69cbff45472
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-04-30 01:55:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-.holder{
-	display:grid;
-	grid-template-columns:repeat(2,100px);
-	grid-template-rows:repeat(2,100px);
+.holder {
+    display: grid;               
+    grid-template-columns: repeat(2, 100px); 
+    grid-template-rows: repeat(2, 100px);    
+    height: 200px;               
 }
-.item-1{
-	background-color:blue;
+
+.item {
+    display: flex;           
+    justify-content: center; 
+    align-items: center;     
+    font-size: 24px;         
+    font-weight: bold;       
 }
-.item-2{
-	background-color:red;
+
+.item-1 {
+    background-color: red;  
 }
-.item-3{
-	background-color:aqua;
+
+.item-2 {
+    background-color: blue;  
 }
-.item-4{
-	background-color:green;
+
+.item-3 {
+    background-color: green;
+}
+
+.item-4 {
+    background-color: yellow; 
 }
